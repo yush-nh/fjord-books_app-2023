0083aaa1bb2c092a402777c53193203d031f0af4
yush-nh <yushin.nihei@luxiar.com>
2024-04-12 10:04:30 +0900

    "add pages of user list, user info"

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users
+  resources :users
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "books#index"
 end
