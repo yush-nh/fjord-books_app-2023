ee7e96ecf7d814da95b4e68dc1f18bce1ad5d29a
yush-nh <yushin.nihei@luxiar.com>
2024-04-17 15:23:16 +0900

    i18n refactor

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: t('books.notice.create') }
+        format.html { redirect_to book_url(@book), notice: t('notice.create', name: Book.model_name.human.downcase) }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('books.notice.update') }
+        format.html { redirect_to book_url(@book), notice: t('notice.update', name: Book.model_name.human.downcase) }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('books.notice.destroy') }
+      format.html { redirect_to books_url, notice: t('notice.destroy', name: Book.model_name.human.downcase) }
       format.json { head :no_content }
     end
   end
