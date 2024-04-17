1f2089aa38150118d42e83a11f95e037e494be8a
yush-nh <yushin.nihei@luxiar.com>
2024-04-18 08:59:14 +0900

    add inverse_of option to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,10 +6,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioning_relations, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy
+  has_many :mentioning_relations, class_name: 'Mention', foreign_key: :mentioning_report_id, inverse_of: :mentioning_report, dependent: :destroy
   has_many :mentioning_reports, through: :mentioning_relations, source: :mentioned_report
 
-  has_many :mentioned_relations, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :mentioned_relations, class_name: 'Mention', foreign_key: :mentioned_report_id, inverse_of: :mentioned_report, dependent: :destroy
   has_many :mentioned_reports, through: :mentioned_relations, source: :mentioning_report
 
   validates :title, presence: true
