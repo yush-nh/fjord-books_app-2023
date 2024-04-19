20857bc4cbd8ff21d69cf48b7fad03f8ffedca64
yush-nh <yushin.nihei@luxiar.com>
2024-04-19 17:38:42 +0900

    add report model spec

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  before do
-    @user = build(:user)
-    @report = build(:report)
+  let(:report) { build(:report) }
+
+  describe '#editable?(target_user)' do
+    context 'userとtarget_userが等しい場合' do
+      it 'trueを返す' do
+        expect(report.editable?(report.user)).to eq true
+      end
+    end
+  end
+
+  describe '#created_on' do
+    it '整形された日時を返す' do
+      expect(report.created_on).to eq(Date.new(2024, 4, 18))
+    end
   end
 end
