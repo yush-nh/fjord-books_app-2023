2bd38a8d8d76946143cb95f2e42a44ef1913d6c5
yush-nh <yushin.nihei@luxiar.com>
2024-04-20 13:17:09 +0900

    removed unnecessary active_storage settings

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_storage.variant_processor = :vips
   end
 end
