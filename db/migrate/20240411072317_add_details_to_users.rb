851bd027e03ca294499e54b09a7fe3aece507600
yush-nh <yushin.nihei@luxiar.com>
2024-04-22 12:19:57 +0900

    unify migration file

--- db/migrate/20240411072317_add_details_to_users.rb
+++ db/migrate/20240411072317_add_details_to_users.rb
@@ -2,6 +2,6 @@ class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :introduction, :text
     add_column :users, :post_code, :string
-    add_column :users, :adress, :string
+    add_column :users, :address, :string
   end
 end
--- db/migrate/20240418045733_rename_adress_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameAdressColumnToUsers < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :adress, :address
-  end
-end
