c998d8ebcde7151c5124a14864cfe9bb5d3fbdd5
yush-nh <yushin.nihei@luxiar.com>
2024-04-22 11:26:12 +0900

    rubocop

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :report do
     title { '日報テスト' }
--- spec/systems/report_spec.rb
+++ spec/systems/report_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.feature '日報機能', type: :system do
